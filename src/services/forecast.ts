633fe30e42d1dae1f613457bf8271a625755c2d1
Guilherme Mariano Munis <guilherme.munis@pampa.compasso>
2021-03-05 17:56:50 -0300

    Implementação do Forecast service agrupamento por hora

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,44 +1,65 @@
 import { ForecastPoint, StormGlass } from '@src/clients/stormGlass';
 
 export enum BeachPosition {
-    S = 'S',
-    E = 'E',
-    W = 'W',
-    N = 'N',
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N',
 }
 
 export interface Beach {
-    name: string;
-    position: BeachPosition;
-    lat: number;
-    lng: number;
-    user: string;
+  name: string;
+  position: BeachPosition;
+  lat: number;
+  lng: number;
+  user: string;
 }
 
-export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[];
+}
+
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint { }
 
 export class Forecast {
-    constructor(protected stormGlass = new StormGlass()) {}
+  constructor(protected stormGlass = new StormGlass()) { }
 
-    public async processForecastForBeaches(
-        beaches: Beach[]
-      ): Promise<BeachForecast[]> {
-        const pointsWithCorrectSources: BeachForecast[] = [];
-        for (const beach of beaches) {
-          const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-          const enrichedBeachData = points.map((e) => ({
-            ...{},
-            ...{
-              lat: beach.lat,
-              lng: beach.lng,
-              name: beach.name,
-              position: beach.position,
-              rating: 1, //need to be implemented
-            },
-            ...e,
-          }));
-          pointsWithCorrectSources.push(...enrichedBeachData);
-        }
-        return pointsWithCorrectSources;
+  public async processForecastForBeaches(
+    beaches: Beach[]
+  ): Promise<TimeForecast[]> {
+    const pointsWithCorrectSources: BeachForecast[] = [];
+    for (const beach of beaches) {
+      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+      const enrichedBeachData = points.map((e) => ({
+        ...{},
+        ...{
+          lat: beach.lat,
+          lng: beach.lng,
+          name: beach.name,
+          position: beach.position,
+          rating: 1, //need to be implemented
+        },
+        ...e,
+      }));
+      pointsWithCorrectSources.push(...enrichedBeachData);
+    }
+    return this.mapForecastByTime(pointsWithCorrectSources);
+  }
+
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = [];
+    for (const point of forecast) {
+      const timepoint = forecastByTime.find((f) => f.time === point.time);
+      if (timepoint) {
+        timepoint.forecast.push(point);
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        });
       }
+    }
+    return forecastByTime;
+  }
 }
