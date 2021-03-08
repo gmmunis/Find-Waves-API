4993e4080885e75204dd0bb6089cd5d20c84c51d
Guilherme Mariano Munis <guilherme.munis@pampa.compasso>
2021-03-08 11:00:38 -0300

    Adicionando teste funcional para o módulo de Beach

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import './util/module-alias';
+import bodyParser from 'body-parser';
 import { Server } from '@overnightjs/core';
 import { Application } from 'express';
-import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
+import { BeachesController } from './controllers/beaches';
 import * as database from '@src/database';
 
 export class SetupServer extends Server {
@@ -24,7 +25,8 @@ export class SetupServer extends Server {
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
   }
 
   public getApp(): Application {
